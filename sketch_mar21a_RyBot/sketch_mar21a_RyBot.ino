d27829e14e39d74dd80a58a78a242cb5873f03c7
mghgpe <mghgpe@gmail.com>
2021-03-22 08:21:17 +0000

    got eyes going neeed to fix paintbrush

--- sketch_mar21a_RyBot/sketch_mar21a_RyBot.ino
+++ sketch_mar21a_RyBot/sketch_mar21a_RyBot.ino
@@ -9,7 +9,84 @@ int switchState;
 int lastSwitchState = LOW;
 
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 1000;    // the debounce time; increase if the output flickers
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+//Set up Morse Code look up table (My first jagged arrays!)
+//First int in each Morse character array is the number of elements in the array starting counting at zero.
+const int a[3] = { 2, 1, 3 };
+const int b[5] = { 4, 3, 1, 1, 1 };
+const int c[5] = { 4, 3, 1, 3, 1 };
+const int d[4] = { 3, 3, 1, 1 };
+const int e[2] = { 1, 1 };
+const int f[5] = { 4, 1, 1, 3, 1 };
+const int g[4] = { 3, 3, 3, 1 };
+const int h[5] = { 4, 1, 1, 1, 1 };
+const int i[3] = { 2, 1, 1 };
+const int j[5] = { 4, 1, 3, 3, 3 };
+const int k[4] = { 3, 3, 1, 3 };
+const int l[5] = { 4, 1, 3, 1, 1 };
+const int m[3] = { 2, 3, 3 };
+const int n[3] = { 2, 3, 1 };
+const int o[4] = { 3, 3, 3, 3 };
+const int p[5] = { 4, 1, 3, 3, 1 };
+const int q[5] = { 4, 3, 3, 1, 3 };
+const int r[4] = { 3, 1, 3, 1 };
+const int s[4] = { 3, 1, 1, 1 };
+const int t[2] = { 1, 3 };
+const int u[4] = { 3, 1, 1, 3 };
+const int v[5] = { 4, 1, 1, 1, 3 };
+const int w[4] = { 3, 1, 3, 3 };
+const int x[5] = { 4, 3, 1, 1, 3 };
+const int y[5] = { 4, 3, 1, 3, 3 };
+const int z[5] = { 4, 3, 3, 1, 1 };
+const int space[2] = { 1, 4 };
+const int one[6] = { 5, 1, 3, 3, 3, 3 };
+const int two[6] = { 5, 1, 1, 3, 3, 3 };
+const int three[6] = { 5, 1, 1, 1, 3, 3 };
+const int four[6] = { 5, 1, 1, 1, 1, 3 };
+const int five[6] = { 5, 1, 1, 1, 1, 1 };
+const int six[6] = { 5, 3, 1, 1, 1, 1 };
+const int seven[6] = { 5, 3, 3, 1, 1, 1 };
+const int eight[6] = { 5, 3, 3, 3, 1, 1 };
+const int nine[6] = { 5, 3, 3, 3, 3, 1 };
+const int zero[6] = { 5, 3, 3, 3, 3, 3 };
+
+//messages (RyBot for EYE_LEDS and buzzer and uncle for HAND_LED and buzzer)
+int const* RyBot[19] = { r, y, b, o, t, space, space, space, s, c, o, t, l, a, n, d, space, space, space }; //Eye leds gpio pin 07
+
+int noElementsInCharacter;
+int timerUnitMultiplier;
+
+
+void sendMessageRyBot() {
+
+  int characterInMessage = 0; //array counting starts at zero
+  int noCharactersInMessage = 19;  //Set for RyBot message array (this has 10 characters)
+  for (characterInMessage; characterInMessage < noCharactersInMessage; // array element are numbered from zero
+      characterInMessage++) {
+
+    int noElementsInCharacter = RyBot[characterInMessage][0];
+    if (RyBot[characterInMessage] == space) {
+      /* Turn LED Off by setting the GPIO pin low  */
+      digitalWrite(eyesLEDS, LOW);
+      delay(space[1] * 100);
+    } else {
+      int i = 1; //first di or dah is at array[1] position
+      for (i; i <= noElementsInCharacter; i++) {
+        timerUnitMultiplier = RyBot[characterInMessage][i];
+        /* Turn LED On by setting the GPIO pin high*/
+        digitalWrite(eyesLEDS, HIGH);
+        delay((timerUnitMultiplier * 100));
+        /* Turn LED Off by setting the GPIO pin low */
+        digitalWrite(eyesLEDS, LOW);
+        delay(100);
+      }
+    }
+    delay(300); //new character delay
+  }
+
+}
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,6 +98,10 @@ void setup() {
 }
 
 void loop() {
+
+//my circuit uses positive logic
+sendMessageRyBot();
+  
   // put your main code here, to run repeatedly:
   // read the state of the switch into a local variable:
   int reading = digitalRead(paintBrushSwitch);
@@ -43,7 +124,11 @@ void loop() {
     if (reading != switchState) {
       switchState = reading;
       ledState = reading;
-  
+       
+       // only toggle the LED if the new button state is HIGH      
+       //if (switchState == HIGH) {
+        //ledState = !ledState;
+      //}
     }
   }
 
@@ -58,9 +143,10 @@ void loop() {
   digitalWrite(blueLED,HIGH);
   delay(wait);
   wait = wait - 10;
-  if(wait < 0) wait = 250; 
-  }
+  if(wait < 0) {
+    wait = 250;
+    break;}
 
       // save the reading. Next time through the loop, it'll be the lastswitchState:
   lastSwitchState = reading;
-}
+  }}
